37d0839bd473bd13a2ef264c7887f5699e2151cd
MiguelÁngel <surah.harus@gmail.com>
2015-02-11 13:43:45 +0100

    Commit

--- FicOnLanWebApi/src/sql/MYSQL_CreateData.sql
+++ FicOnLanWebApi/src/sql/MYSQL_CreateData.sql
@@ -1,3 +1,3 @@
 INSERT INTO EmailTemplate VALUES ( 0, "passwordRecover", "", "", "Recuperación contraseña plataforma Freak's Party webs",
  "La nueva contraseña para la cuenta #loginusuario es #nuevapas esta contraseña solo será válida durante #tiemporestante minutos, recuerda cambiarla.");
- 
+  
