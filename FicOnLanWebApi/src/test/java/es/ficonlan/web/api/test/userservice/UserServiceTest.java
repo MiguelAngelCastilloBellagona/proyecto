ffd9f06de06febc7b1295c30c494cf21a304f376
MiguelAngelCastilloBellagona <surah.harus@gmail.com>
2015-02-22 13:44:47 +0100

    Test de UserService al 50%

--- FicOnLanWebApi/src/test/java/es/ficonlan/web/api/test/userservice/UserServiceTest.java
+++ FicOnLanWebApi/src/test/java/es/ficonlan/web/api/test/userservice/UserServiceTest.java
@@ -4,9 +4,13 @@ import static es.ficonlan.web.api.model.util.GlobalNames.SPRING_CONFIG_FILE;
 import static es.ficonlan.web.api.test.util.GlobalNames.SPRING_CONFIG_TEST_FILE;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
+import java.util.Properties;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +19,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import es.ficonlan.web.api.jersey.Main;
 import es.ficonlan.web.api.model.emailtemplate.EmailTemplate;
 import es.ficonlan.web.api.model.emailtemplate.EmailTemplateDao;
+import es.ficonlan.web.api.model.session.Session;
+import es.ficonlan.web.api.model.session.SessionDao;
 import es.ficonlan.web.api.model.user.User;
 import es.ficonlan.web.api.model.user.UserDao;
 import es.ficonlan.web.api.model.userService.UserService;
@@ -30,12 +37,30 @@ import es.ficonlan.web.api.model.util.exceptions.ServiceException;
 @ContextConfiguration(locations = {SPRING_CONFIG_FILE, SPRING_CONFIG_TEST_FILE})
 @Transactional
 public class UserServiceTest {
+	
+	private static Properties properties;
+
+	static {
+		try {
+			properties = new Properties();
+			InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("backend.properties");
+			properties.load(inputStream);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not read config file: " + e.getMessage());
+		}
+	}
+
+	private static final int SESSION_TIMEOUT = Math.abs(Integer.parseInt(properties.getProperty("session.timeout")));
+	
 
 	@Autowired
     private UserService userService;
     
 	@Autowired
 	private UserDao userDao;
+	
+	@Autowired
+	private SessionDao sessionDao;
 	
 	@Autowired
 	private EmailTemplateDao emailTemplateDao;
@@ -52,6 +77,25 @@ public class UserServiceTest {
 	@Test
 	public void correctTest() {
 		
+	}
+	
+	
+	@Test
+	public void closeOldSessionsTest() throws ServiceException, InstanceException {
+		User user = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user);
+	    Session s = sessionDao.find(userService.login("login", "password").getSessionId());
+	    userService.closeOldSessions();
+	    sessionDao.find(s.getSessionId());
+	    
+	    Calendar now = Calendar.getInstance();
+	    now.add(Calendar.MINUTE, (SESSION_TIMEOUT*-1)-1);
+	    s.setLastAccess(now);
+	    userService.closeOldSessions();
+	    try {
+	    	sessionDao.find(s.getSessionId());
+	    	Assert.fail();
+	    } catch (InstanceException e) { }
 	}
 	
 	@Test
@@ -71,5 +115,113 @@ public class UserServiceTest {
 		assertFalse(userService.passwordRecover("Noemail@yopmails.com"));
 	}
 	
-  
+	@Test
+	public void addUserTest() throws InstanceException, ServiceException {
+		User user = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user);
+	    User u1 = userDao.find(user.getUserId());
+	    Assert.assertEquals("User", u1.getName());
+	    User user2 = new User("", "User", "login", "password", "00000000B", "email2@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    try {
+			userService.addUser(user2);
+			Assert.fail();
+		} catch (ServiceException e) {}
+	    User user3 = new User("", "User", "login2", "password", "00000000A", "email2@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    try {
+			userService.addUser(user3);
+			Assert.fail();
+		} catch (ServiceException e) {}
+	    User user4 = new User("", "User", "login2", "password", "00000000B", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    try {
+			userService.addUser(user4);
+			Assert.fail();
+		} catch (ServiceException e) {}  
+	}
+	
+	@Test
+	public void loginTest() throws ServiceException {
+		try {
+			userService.login("login", "password");
+			Assert.fail();
+		} catch (ServiceException e) {}
+		User user = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user);
+	    try {
+			userService.login("login2", "password");
+			Assert.fail();
+		} catch (ServiceException e) {}
+	    try {
+			userService.login("login", "password2");
+			Assert.fail();
+		} catch (ServiceException e) {}
+	    try {
+			userService.login("login2", "password2");
+			Assert.fail();
+		} catch (ServiceException e) {}
+
+	    userService.login("login", "password");   
+	}
+ 
+	@Test
+	public void setSessionLastAccessNowTest() throws InstanceException, ServiceException {
+		User user = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user);
+	    Session s = sessionDao.find(userService.login("login", "password").getSessionId());
+	    Calendar n = Calendar.getInstance();
+	    n.add(Calendar.MINUTE, 14);
+	    s.setLastAccess(n);
+	    Assert.assertEquals(n.getTime(), s.getLastAccess().getTime());
+	    sessionDao.save(s);
+	    userService.setSessionLastAccessNow(s.getSessionId());
+	    Assert.assertEquals(Calendar.getInstance().getTime(), s.getLastAccess().getTime());
+	}
+	
+	@Test
+	public void sessionExistsTest() throws InstanceException, ServiceException {
+		User user = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user);
+	    String sessionId = userService.login("login", "password").getSessionId();
+	    Assert.assertTrue(userService.sessionExists(sessionId));
+	    sessionDao.remove(sessionId);
+	    Assert.assertFalse(userService.sessionExists(sessionId));
+	}
+	
+	@Test
+	public void getCurrenUserUSERTest() throws ServiceException {
+		User user = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user);
+	    String sessionId = userService.login("login", "password").getSessionId();
+	    User user2 = userService.getCurrenUserUSER(sessionId);
+	    Assert.assertEquals(user.getUserId(), user2.getUserId());
+	}
+	
+	@Test
+	public void removeUserUSERTest() throws ServiceException {
+		User user = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user);
+	    String sessionId = userService.login("login", "password").getSessionId();
+	    userService.removeUserUSER(sessionId);
+	    try {
+			userService.removeUserUSER(sessionId);
+			Assert.fail();
+		} catch (ServiceException e) {}
+	}
+	
+	@Test
+	public void changeUserDataUSERTest () throws ServiceException {
+		User user1 = new User("", "User", "login", "password", "00000000A", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user1);
+	    User user2 = new User("", "User2", "login2", "password2", "00000000B", "email2@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    userService.addUser(user2);
+	    String sessionId = userService.login("login2", "password2").getSessionId();
+	    User userData = new User("", "User", "login", "password", "00000000C", "email@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+	    try {
+			userService.changeUserDataUSER(sessionId, userData);
+			Assert.fail();
+		} catch (ServiceException e) {}
+	    userData = new User("", "User", "login3", "password", "00000000C", "email3@yopmail.com", "666666666", "XL", Calendar.getInstance(), "EN");
+		userService.changeUserDataUSER(sessionId, userData);
+
+		
+	}
 }
