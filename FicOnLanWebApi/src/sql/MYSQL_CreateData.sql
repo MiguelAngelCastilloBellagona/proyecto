cac05ebcbfbb875957f964b9adc393b5962acce4
Miguel Angel Castillo Bellagona <surah.harus@gmail.com>
2015-03-21 14:17:00 +0100

    Nuevo import

--- FicOnLanWebApi/src/sql/MYSQL_CreateData.sql
+++ FicOnLanWebApi/src/sql/MYSQL_CreateData.sql
@@ -1,4 +1,4 @@
 INSERT INTO EmailTemplate VALUES ( 0, "passwordRecover", "", "", "Recuperación contraseña plataforma Freak's Party webs",
  "La nueva contraseña para la cuenta #loginusuario es #nuevapas esta contraseña solo será válida durante #tiemporestante minutos, recuerda cambiarla.");
   
-   
+  
